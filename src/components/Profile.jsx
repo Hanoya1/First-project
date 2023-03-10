625a3171e3d75d5940d028560a3c0453aee7e9a1
Matvei <leshinskymatvey@gmail.com>
2023-03-10 13:41:24 +0100

    New project

---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+const Profile = () => {
+   return (
+      <div className='content'>
+         <div>
+            <img src='https://images.unsplash.com/photo-1543304216-b46be324b571?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2181&q=80'></img>
+         </div>
+         <div>
+            ava + desc
+         </div>
+         <div>
+            my posts
+            <div>
+               new post
+            </div>
+         </div>
+         <div>
+            <div>post 1</div>
+            <div>post 2</div>
+         </div>
+      </div>
+   );
+}
+
+export default Profile
